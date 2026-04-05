3df470d0664d357d0bef37ebed7ef7329a4bbf35
Jiahao, Woo <woojiahao1234@gmail.com>
2026-04-05 16:51:51 +0800

    Implement configuration model

--- db/models/configuration.go
+++ db/models/configuration.go
@@ -3,6 +3,8 @@ package models
 import (
 	"database/sql"
 	"time"
+
+	"github.com/woojiahao/daily-planet/db/scanner"
 )
 
 type Configuration struct {
@@ -17,4 +19,65 @@ type Configuration struct {
 
 type ConfigurationModel struct {
 	DB *sql.DB
+}
+
+func parseConfigurationRow(rows scanner.RowScanner) (Configuration, error) {
+	var configuration Configuration
+	var showStatsInt int
+	var disabledInt int
+	var createdAtString string
+
+	err := rows.Scan(
+		&configuration.ID,
+		&configuration.SnowflakeID,
+		&configuration.Type,
+		&configuration.CronSchedule,
+		&showStatsInt,
+		&disabledInt,
+		&createdAtString,
+	)
+	if err != nil {
+		return Configuration{}, err
+	}
+
+	configuration.Disabled = disabledInt == 1
+	configuration.ShowStats = showStatsInt == 1
+	createdAt, err := time.Parse("2006-01-02 15:04:05", createdAtString)
+	if err != nil {
+		return Configuration{}, err
+	}
+	configuration.CreatedAt = createdAt
+
+	return configuration, nil
+}
+
+func (m ConfigurationModel) All() ([]Configuration, error) {
+	query := `
+	SELECT
+		id,
+		snowflake_id,
+		type,
+		cron_schedule,
+		show_stats,
+		disabled,
+		created_at
+	FROM
+		configuration;`
+	rows, err := m.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var configurations []Configuration
+	for rows.Next() {
+		configuration, err := parseConfigurationRow(rows)
+		if err != nil {
+			return nil, err
+		}
+		configurations = append(configurations, configuration)
+	}
+
+	return configurations, nil
 }
