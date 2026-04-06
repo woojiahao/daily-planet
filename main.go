0f02c289b53faf1a89303f4c566c80c645d9a13b
Jiahao, Woo <woojiahao1234@gmail.com>
2026-04-06 21:13:10 +0800

    Use bot infrastructure to start bot

--- main.go
+++ main.go
@@ -1,10 +1,23 @@
 package main
 
-import "github.com/joho/godotenv"
+import (
+	"github.com/joho/godotenv"
+	"github.com/woojiahao/daily-planet/bot"
+	"github.com/woojiahao/daily-planet/db"
+)
+
+var database db.Database
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		panic(err)
 	}
+
+	// database, err := db.New()
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	bot.Run()
 }
