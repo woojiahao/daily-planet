f49b29a86de9bde46649efcf6fbe9740a4c7b8e2
Jiahao, Woo <woojiahao1234@gmail.com>
2026-04-08 08:53:18 +0800

    Use color type

--- bot/helpers/response.go
+++ bot/helpers/response.go
@@ -1,7 +1,9 @@
 // Package helpers provides helpers over the discordgo API to reduce duplication
 package helpers
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"github.com/bwmarrin/discordgo"
+)
 
 func SendMessage(s *discordgo.Session, i *discordgo.InteractionCreate, content string) {
 	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
@@ -12,7 +14,7 @@ func SendMessage(s *discordgo.Session, i *discordgo.InteractionCreate, content s
 	})
 }
 
-func SendEmbed(s *discordgo.Session, i *discordgo.InteractionCreate, title, description string, color int) {
+func SendEmbed(s *discordgo.Session, i *discordgo.InteractionCreate, title, description string, color Color) {
 	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
 		Data: &discordgo.InteractionResponseData{
@@ -20,7 +22,7 @@ func SendEmbed(s *discordgo.Session, i *discordgo.InteractionCreate, title, desc
 				{
 					Title:       title,
 					Description: description,
-					Color:       color,
+					Color:       int(color),
 				},
 			},
 		},
