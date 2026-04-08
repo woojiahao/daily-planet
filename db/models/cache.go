1b829e777f3434f1f50b65b470241be690847176
Jiahao, Woo <woojiahao1234@gmail.com>
2026-04-08 08:53:38 +0800

    Support insert many cache to avoid multiple INSERT

--- db/models/cache.go
+++ db/models/cache.go
@@ -1,6 +1,10 @@
 package models
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+	"strings"
+)
 
 type Cache struct {
 	ID              int
@@ -32,6 +36,42 @@ func (m CacheModel) InsertOne(configurationID, feedID int, articleKey string) er
 	defer stmt.Close()
 
 	_, err = stmt.Exec(configurationID, feedID, articleKey)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (m CacheModel) InsertMany(configurationIDs, feedIDs []int, articleKeys []string) error {
+	if len(configurationIDs) != len(feedIDs) || len(configurationIDs) != len(articleKeys) || len(feedIDs) != len(articleKeys) {
+		return fmt.Errorf("input should be of equal length")
+	}
+
+	var placeholderValues []any
+	var queryPlaceholders []string
+
+	for i := range len(configurationIDs) {
+		placeholderValues = append(placeholderValues, configurationIDs[i], feedIDs[i], articleKeys[i])
+		queryPlaceholders = append(queryPlaceholders, "(?, ?, ?)")
+	}
+
+	query := fmt.Sprintf(`
+	INSERT INTO cache (
+		configuration_id,
+		feed_id,
+		article_key
+	) VALUES 
+		%s
+	;`, strings.Join(queryPlaceholders, ",\n"))
+	stmt, err := m.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	_, err = stmt.Exec(placeholderValues...)
 	if err != nil {
 		return err
 	}
