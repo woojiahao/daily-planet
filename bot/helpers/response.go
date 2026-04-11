4f3053d2fefd36bc8491d960ade4f32502bad433
Jiahao, Woo <woojiahao1234@gmail.com>
2026-04-11 09:53:50 +0800

    Improve ephemeral response handling

--- bot/helpers/response.go
+++ bot/helpers/response.go
@@ -40,6 +40,37 @@ func CreateModal(customID, title string, components []discordgo.MessageComponent
 	}
 }
 
+func CreateMessageComponent(content string, components []discordgo.MessageComponent) *discordgo.InteractionResponse {
+	return &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content:    content,
+			Components: components,
+		},
+	}
+}
+
+func CreateEphemeralMessage(content string) *discordgo.InteractionResponse {
+	return createEphemeralResponse(CreateMessage(content))
+}
+
+func CreateEphemeralEmbed(title, description string, color Color) *discordgo.InteractionResponse {
+	return createEphemeralResponse(CreateEmbed(title, description, color))
+}
+
+func CreateEphemeralModal(customID, title string, components []discordgo.MessageComponent) *discordgo.InteractionResponse {
+	return createEphemeralResponse(CreateModal(customID, title, components))
+}
+
+func CreateEphemeralMessageComponent(content string, components []discordgo.MessageComponent) *discordgo.InteractionResponse {
+	return createEphemeralResponse(CreateMessageComponent(content, components))
+}
+
+func createEphemeralResponse(response *discordgo.InteractionResponse) *discordgo.InteractionResponse {
+	response.Data.Flags = discordgo.MessageFlagsEphemeral
+	return response
+}
+
 func SendMessage(s *discordgo.Session, i *discordgo.InteractionCreate, content string) {
 	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
