7c33cb38a61956ef36131b92bcacf7120a3b3bb2
Jiahao, Woo <woojiahao1234@gmail.com>
2026-04-12 11:23:24 +0800

    Add disable/enable commands

---
+++ bot/commands/enable_feed.go
@@ -0,0 +1,56 @@
+package commands
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/woojiahao/daily-planet/bot/context"
+	"github.com/woojiahao/daily-planet/bot/helpers"
+)
+
+var EnableFeed = Command{
+	Name:        "enable-feed",
+	Description: "Enable a provided feed",
+	Options: []*discordgo.ApplicationCommandOption{
+		{
+			Type:        discordgo.ApplicationCommandOptionString,
+			Name:        "url",
+			Description: "Feed URL to add",
+			Required:    true,
+		},
+	},
+	Handler: func(context context.CommandContext) *discordgo.InteractionResponse {
+		url := helpers.GetRequiredOption[string](context, "url")
+		feed, err := context.Database.Feed.OneByConfigurationIDAndURL(context.CallerConfiguration.ID, url)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				return helpers.CreateEmbed(
+					"Feed not found",
+					fmt.Sprintf("Failed to fetch feed by URL %s as it does not exist.\n\nUse /list-feeds to verify that it exists in this source.", url),
+					helpers.ColorRed,
+				)
+			}
+			return helpers.CreateEmbed(
+				"Failed to fetch feed",
+				fmt.Sprintf("Failed to fetch feed by URL %s. Try again", url),
+				helpers.ColorRed,
+			)
+		}
+
+		err = context.Database.Feed.UpdateOneByID(feed.ID, false)
+		if err != nil {
+			return helpers.CreateEmbed(
+				"Failed to enable feed",
+				fmt.Sprintf("Failed to enable feed by URL %s. Try again", url),
+				helpers.ColorRed,
+			)
+		}
+
+		return helpers.CreateEmbed(
+			"Feed enabled",
+			fmt.Sprintf("Feed %s enabled", url),
+			helpers.ColorGreen,
+		)
+	},
+}
