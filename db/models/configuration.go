3b235728292910a20d19e757add9c55201c4c561
Jiahao, Woo <woojiahao1234@gmail.com>
2026-04-15 08:58:10 +0800

    Add OneByID for configuration

--- db/models/configuration.go
+++ db/models/configuration.go
@@ -94,6 +94,38 @@ func (m ConfigurationModel) All() ([]Configuration, error) {
 	return configurations, nil
 }
 
+func (m ConfigurationModel) OneByID(id ConfigurationID) (Configuration, error) {
+	query := `
+	SELECT
+		id,
+		snowflake_id,
+		type,
+		cron_schedule,
+		show_stats,
+		disabled,
+		created_at
+	FROM
+		configuration
+	WHERE
+		id = ?
+	LIMIT 1;`
+	stmt, err := m.DB.Prepare(query)
+	if err != nil {
+		return Configuration{}, err
+	}
+
+	defer stmt.Close()
+
+	row := stmt.QueryRow(id)
+
+	configuration, err := parseConfigurationRow(row)
+	if err != nil {
+		return Configuration{}, err
+	}
+
+	return configuration, nil
+}
+
 func (m ConfigurationModel) OneBySnowflakeID(snowflakeID string) (Configuration, error) {
 	query := `
 	SELECT
