dd47055196176dfcbc6b3a5c5fba9735443d1f80
Jiahao, Woo <woojiahao1234@gmail.com>
2026-05-03 13:42:43 +0800

    Migrate disable configuration to use transaction

--- bot/commands/disable_configuration.go
+++ bot/commands/disable_configuration.go
@@ -1,12 +1,18 @@
 package commands
 
 import (
-	"fmt"
+	"errors"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/woojiahao/daily-planet/bot/context"
 	"github.com/woojiahao/daily-planet/bot/helpers"
 	"github.com/woojiahao/daily-planet/common"
+	"github.com/woojiahao/daily-planet/db"
+)
+
+var (
+	errDisableConfigurationUpdateFailed         = errors.New("failed to update configuration row")
+	errDisableConfigurationCancelScheduleFailed = errors.New("failed to cancel configuration schedule")
 )
 
 var DisableConfiguration = Command{
@@ -15,29 +21,42 @@ var DisableConfiguration = Command{
 	Description: "Disable this source from posting updates",
 	Handler: func(context context.CommandContext) *discordgo.InteractionResponse {
 		disabled := true
-		_, err := context.Database.Configuration.UpdateOneByID(context.CallerConfiguration.ID, nil, nil, nil, &disabled)
-		if err != nil {
-			fmt.Printf("%v\n", err)
-			return helpers.CreateSimpleEmbed(
+		configurationID := context.CallerConfiguration.ID
+
+		err := context.Database.WithTransaction(func(tx db.Database) error {
+			if _, err := tx.Configuration.UpdateOneByID(
+				configurationID,
+				nil,
+				nil,
+				nil,
+				&disabled,
+			); err != nil {
+				return errDisableConfigurationUpdateFailed
+			}
+
+			if err := context.Scheduler.Cancel(configurationID); err != nil {
+				return errDisableConfigurationCancelScheduleFailed
+			}
+
+			return nil
+		})
+
+		return common.SwitchError(err, map[error]*discordgo.InteractionResponse{
+			nil: helpers.CreateSimpleEmbed(
+				"Configuration disabled",
+				"Configuration for this source has been disabled.\n\nYou will no longer see updates in this source.",
+				common.ColorGreen,
+			),
+			errDisableConfigurationUpdateFailed: helpers.CreateSimpleEmbed(
 				"Failed to disable configuration",
 				"Failed to disable configuration for this source",
 				common.ColorRed,
-			)
-		}
-
-		if err = context.Scheduler.Cancel(context.CallerConfiguration.ID); err != nil {
-			fmt.Printf("%v\n", err)
-			return helpers.CreateSimpleEmbed(
+			),
+			errDisableConfigurationCancelScheduleFailed: helpers.CreateSimpleEmbed(
 				"Failed to cancel configuration's schedule",
 				"Failed to disable configuration's schedule for this source",
 				common.ColorRed,
-			)
-		}
-
-		return helpers.CreateSimpleEmbed(
-			"Configuration disabled",
-			"Configuration for this source has been disabled.\n\nYou will no longer see updates in this source.",
-			common.ColorGreen,
-		)
+			),
+		})
 	},
 }
