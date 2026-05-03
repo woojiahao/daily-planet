0ad08f6250752b91b1bfcdfabea75416648c720d
Jiahao, Woo <woojiahao1234@gmail.com>
2026-05-03 12:34:06 +0800

    Defer upload operation

--- bot/commands/upload_feeds.go
+++ bot/commands/upload_feeds.go
@@ -28,71 +28,87 @@ var UploadFeeds = Command{
 		// TODO(woojiahao): wrap these in a transaction instead of separating the API calls
 		yamlConfig := helpers.GetRequiredOption[[]byte](context, "feeds_file")
 
-		var rawFeedData struct {
-			Feeds []string `yaml:"feeds"`
-		}
-
-		if err := yaml.Unmarshal(yamlConfig, &rawFeedData); err != nil {
-			panic(err)
-		}
-
-		urls := rawFeedData.Feeds
-
-		feeds := source.BulkLoadFeeds(urls)
-
-		var feedURLs []string
-		var feedTypes []string
-		for _, feed := range feeds {
-			// if no title, means it didn't load so skip
-			if feed.Title == "" {
-				continue
+		go func() {
+			var rawFeedData struct {
+				Feeds []string `yaml:"feeds"`
 			}
-			feedURLs = append(feedURLs, feed.RawLink)
-			feedTypes = append(feedTypes, string(feed.EngineType))
-		}
-		fmt.Printf("%v\n", feedTypes)
 
-		dbFeeds, err := context.Database.Feed.InsertManyWithSameConfigurationID(context.CallerConfiguration.ID, feedURLs, feedTypes)
-		if err != nil {
-			fmt.Printf("err is %v\n", err)
-			return helpers.CreateSimpleEmbed(
-				"Feeds NOT uploaded",
-				"Failed to upload feeds to source. Try again.",
-				common.ColorRed,
-			)
-		}
-
-		dbFeedIDsByURL := make(map[string]models.FeedID)
-		for _, f := range dbFeeds {
-			dbFeedIDsByURL[f.URL] = f.ID
-		}
-
-		var articleKeys []string
-		var cacheKeys []models.CacheKey
-		// for all articles, bulk insert them into the cache
-		for _, feed := range feeds {
-			for _, article := range feed.Articles {
-				articleKeys = append(articleKeys, string(article.GetKey()))
-				cacheKeys = append(cacheKeys, models.NewCacheKey(context.CallerConfiguration.ID, dbFeedIDsByURL[feed.RawLink]))
+			if err := yaml.Unmarshal(yamlConfig, &rawFeedData); err != nil {
+				helpers.SendSimpleEmbed(
+					context.Session,
+					context.Interaction,
+					"Failed to load YAML",
+					"Invalid YAML provided",
+					common.ColorRed,
+				)
 			}
-		}
-		err = context.Database.Cache.InsertMany(
-			cacheKeys,
-			articleKeys,
-		)
-		if err != nil {
-			fmt.Printf("err is %v\n", err)
-			return helpers.CreateSimpleEmbed(
-				"Feeds NOT uploaded",
-				"Failed to add feed articles into cache",
-				common.ColorRed,
-			)
-		}
 
-		return helpers.CreateSimpleEmbed(
-			"Feed added",
-			"Uploaded feeds to source",
-			common.ColorGreen,
-		)
+			urls := rawFeedData.Feeds
+
+			feeds := source.BulkLoadFeeds(urls)
+
+			var feedURLs []string
+			var feedTypes []string
+			for _, feed := range feeds {
+				// if no title, means it didn't load so skip
+				if feed.Title == "" {
+					continue
+				}
+				feedURLs = append(feedURLs, feed.RawLink)
+				feedTypes = append(feedTypes, string(feed.EngineType))
+			}
+			fmt.Printf("%v\n", feedTypes)
+
+			dbFeeds, err := context.Database.Feed.InsertManyWithSameConfigurationID(context.CallerConfiguration.ID, feedURLs, feedTypes)
+			if err != nil {
+				fmt.Printf("err is %v\n", err)
+				helpers.SendSimpleEmbed(
+					context.Session,
+					context.Interaction,
+					"Feeds NOT uploaded",
+					"Failed to upload feeds to source. Try again.",
+					common.ColorRed,
+				)
+			}
+
+			dbFeedIDsByURL := make(map[string]models.FeedID)
+			for _, f := range dbFeeds {
+				dbFeedIDsByURL[f.URL] = f.ID
+			}
+
+			var articleKeys []string
+			var cacheKeys []models.CacheKey
+			// for all articles, bulk insert them into the cache
+			for _, feed := range feeds {
+				for _, article := range feed.Articles {
+					articleKeys = append(articleKeys, string(article.GetKey()))
+					cacheKeys = append(cacheKeys, models.NewCacheKey(context.CallerConfiguration.ID, dbFeedIDsByURL[feed.RawLink]))
+				}
+			}
+			err = context.Database.Cache.InsertMany(
+				cacheKeys,
+				articleKeys,
+			)
+			if err != nil {
+				fmt.Printf("err is %v\n", err)
+				helpers.SendSimpleEmbed(
+					context.Session,
+					context.Interaction,
+					"Feeds NOT uploaded",
+					"Failed to add feed articles into cache",
+					common.ColorRed,
+				)
+			}
+
+			helpers.SendSimpleEmbed(
+				context.Session,
+				context.Interaction,
+				"Feed added",
+				"Uploaded feeds to source",
+				common.ColorGreen,
+			)
+		}()
+
+		return helpers.CreateDeferredResponse()
 	},
 }
